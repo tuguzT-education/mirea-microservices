5ed6483a6017aac916ba3f3fadbb6a589bf8b9b3
tuguzT <timurka.tugushev@gmail.com>
2022-11-24 11:51:26 +0300

    Fix schema for new microservice

--- crates/cringy-blog-blogs/src/schema.rs
+++ crates/cringy-blog-blogs/src/schema.rs
@@ -1,11 +1,5 @@
 // @generated automatically by Diesel CLI.
 
-pub mod sql_types {
-    #[derive(diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "task_completion_state"))]
-    pub struct TaskCompletionState;
-}
-
 diesel::table! {
     blogs (blog_id) {
         blog_id -> Uuid,
@@ -13,21 +7,3 @@ diesel::table! {
         name -> Varchar,
     }
 }
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::TaskCompletionState;
-
-    tasks (task_id) {
-        task_id -> Uuid,
-        blog_id -> Uuid,
-        name -> Varchar,
-        deadline -> Nullable<Timestamptz>,
-        completion -> TaskCompletionState,
-    }
-}
-
-diesel::allow_tables_to_appear_in_same_query!(
-    blogs,
-    tasks,
-);
